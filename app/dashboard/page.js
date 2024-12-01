f74a157bb2a6db2cbe6d6ad9bbda1171e93f5be5
Don Phelix <141995407+dondevteam@users.noreply.github.com>
2024-12-01 06:19:38 +0300

    implemented chart section and insights display

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import {VStack, Heading} from "@chakra-ui/react";
-// import ChartsSection from "@/components/ChartsSection";
+import ChartsSection from "@/components/ChartsSection";
 // import TransactionTable from "@/components/TransactionTable";
 import {generateMockData} from "@/utils/mockData";
 import KPIGrid from "@/components/KPIGrid";
@@ -14,12 +14,12 @@ export default function DashboardPage() {
         <VStack spacing={8} p={6}>
             <Heading>Business Performance Dashboard</Heading>
             <KPIGrid data={{sales, expenses}}/>
-            {/*<ChartsSection*/}
-            {/*    sales={sales}*/}
-            {/*    expenses={expenses}*/}
-            {/*    revenueByRegion={revenueByRegion}*/}
-            {/*    expensesByCategory={expensesByCategory}*/}
-            {/*/>*/}
+            <ChartsSection
+                sales={sales}
+                expenses={expenses}
+                revenueByRegion={revenueByRegion}
+                expensesByCategory={expensesByCategory}
+            />
             {/*<TransactionTable transactions={transactions} />*/}
         </VStack>
     );
